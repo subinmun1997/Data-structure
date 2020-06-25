ac0035dd94883a138512fa1c0edf98f45a0dce83
subinmun1997 <qzxy812@gmail.com>
2020-06-25 16:09:01 +0900

    Circle Linked List

---
+++ CLinkedList.h/CLinkedList.h/CLinkedList.h
@@ -0,0 +1,33 @@
+#pragma once
+#ifndef __C_LINKED_LIST_H__
+#define __C_LINKED_LIST_H__
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int Data;
+
+typedef struct _node {
+	Data data;
+	struct _node* next;
+}Node;
+
+typedef struct _CLL {
+	Node* tail;
+	Node* cur;
+	Node* before;
+	int numOfData;
+}CList;
+
+typedef CList List;
+
+void ListInit(List* plist);
+void ListInsert(List* plist, Data data);
+void ListInsertFront(List* plist, Data data);
+
+int LFirst(List* plist, Data* pdata);
+int LNext(List* plist, Data* pdata);
+Data LRemove(List* plist);
+int LCount(List* plist);
+
+#endif
